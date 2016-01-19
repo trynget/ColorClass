46a56fc24e694d77134f34d73b973a9da87e1939
= <=>
2016-01-19 11:07:59 +0800

    更新

---
+++ www/js/controllers.js
@@ -0,0 +1,47 @@
+angular.module('starter.controllers', [])
+//在这下面添加页面对应的控制器，每个控制器管理对应页面的功能，跳转等等
+  .controller('startCtrl', function($scope, $state, showMsgService) {
+    showMsgService.showMsg("首页控制器是startCtrl");
+    $scope.goFirst = function() {
+      $state.go('first');
+    }
+  })
+  .controller('firstCtrl',function($scope ,$state) {
+        $scope.goSecond_1 = function() {
+          $state.go('second_1');
+        };
+        $scope.goSecond_2 = function() {
+            $state.go('second_2');
+        };
+        $scope.goSecond_3 = function() {
+            $state.go('second_3');
+        };
+        $scope.goSecond_4 = function() {
+            $state.go('second_4');
+        }
+  })
+    //.controller('firstCtrl',function($scope ,$state) {
+    //    $scope.goBackStart = function() {
+    //        $state.go('start');
+    //    }
+    //})
+.controller('Second_1Ctrl',function($scope ,$state) {
+    $scope.backFirst = function() {
+        $state.go('first');
+    }
+})
+.controller('Second_2Ctrl',function($scope ,$state) {
+    $scope.backFirst = function() {
+        $state.go('first');
+    }
+})
+.controller('Second_3Ctrl',function($scope ,$state) {
+    $scope.backFirst = function() {
+        $state.go('first');
+    }
+})
+.controller('Second_4Ctrl',function($scope ,$state) {
+    $scope.backFirst = function() {
+        $state.go('first');
+    }
+});
